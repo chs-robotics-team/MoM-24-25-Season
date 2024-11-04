0df737be89dfe3a376ac1c84d9bebe2e47a3ad57
MoM-laptop <ministryofmechanics.laptop@gmail.com>
2024-11-04 18:38:57 -0500

    feat: servos move

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotHardware.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotHardware.kt
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.util
 
 import com.arcrobotics.ftclib.drivebase.MecanumDrive
+import com.arcrobotics.ftclib.gamepad.ButtonReader
 import com.arcrobotics.ftclib.gamepad.GamepadEx
 import com.arcrobotics.ftclib.gamepad.GamepadKeys
 import com.arcrobotics.ftclib.gamepad.ToggleButtonReader
+import com.arcrobotics.ftclib.hardware.motors.CRServo
 import com.arcrobotics.ftclib.hardware.motors.Motor
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.teamcode.opmodes.logger
@@ -41,12 +43,30 @@ class DriveTrain(private val hardware: RobotHardware) : MecanumDrive(
     }
 }
 
+class Intake(private val hardware: RobotHardware) {
+    fun spin() {
+        logger.debug("Spin: ${hardware.doSpinIntake.isDown}")
+        if (hardware.doSpinIntake.isDown) {
+            hardware.leftIntakeServo.set(1.0)
+            hardware.rightIntakeServo.set(-1.0)
+        } else {
+            hardware.leftIntakeServo.set(0.0)
+            hardware.rightIntakeServo.set(0.0)
+        }
+    }
+}
+
 class RobotHardware(val hardwareMap: HardwareMap, val gamepad: GamepadEx) {
+    val leftIntakeServo = CRServo(hardwareMap, "lIntakeServo")
+    val rightIntakeServo = CRServo(hardwareMap, "rIntakeServo")
     val frontLeftMotor = Motor(hardwareMap, "flMotor", Motor.GoBILDA.RPM_435)
     val frontRightMotor = Motor(hardwareMap, "frMotor", Motor.GoBILDA.RPM_435)
     val backLeftMotor = Motor(hardwareMap, "blMotor", Motor.GoBILDA.RPM_435)
     val backRightMotor = Motor(hardwareMap, "brMotor", Motor.GoBILDA.RPM_435)
     val driveTrain = DriveTrain(this)
+    val intake = Intake(this);
+
+    val doSpinIntake = ButtonReader(gamepad, GamepadKeys.Button.RIGHT_BUMPER)
 
     val toggleFast = ToggleButtonReader(gamepad, GamepadKeys.Button.RIGHT_STICK_BUTTON)
     val toggleHang = ToggleButtonReader(gamepad, GamepadKeys.Button.BACK)
